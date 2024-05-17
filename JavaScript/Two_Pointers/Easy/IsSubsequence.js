e3f92a1ddebf9b508b9889aad9296858b3bb3829
tbohn2 <tanner.bohn@yahoo.com>
2024-05-17 11:24:13 -0700

    Is Subsequence

---
+++ JavaScript/Two_Pointers/Easy/IsSubsequence.js
@@ -0,0 +1,22 @@
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without 
+// disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+// While loop to move through both arrays, only advancing in s if the character in t matches
+var isSubsequence = function (s, t) {
+    let sIndex = 0;
+    let tIndex = 0;
+
+    while (sIndex < s.length && tIndex < t.length) {
+        if (s[sIndex] === t[tIndex]) {
+            sIndex++;
+        }
+        tIndex++;
+    }
+
+    return sIndex === s.length;
+};
+
+console.log(isSubsequence('abc', 'ahbgdc'));
+console.log(isSubsequence('axc', 'ahbgdc'));
