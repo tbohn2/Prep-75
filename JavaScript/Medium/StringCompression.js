0a0b0e8972d2703c6fff76192ea82dc05a43c1b6
tbohn2 <tanner.bohn@yahoo.com>
2024-05-17 10:28:12 -0700

    String Compression

---
+++ JavaScript/Medium/StringCompression.js
@@ -0,0 +1,38 @@
+// Given an array of characters chars, compress it using the following algorithm:
+
+// Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+// If the group's length is 1, append the character to s.
+// Otherwise, append the character followed by the group's length.
+// The compressed string s should not be returned separately, but instead, be stored in the input character array chars.
+//  Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+// After you are done modifying the input array, return the new length of the array.
+
+// You must write an algorithm that uses only constant extra space.
+
+var compress = function (chars) {
+    let s = ''
+    let numberOfChars = 0
+    for (let i = 0; i < chars.length; i++) {
+        numberOfChars++
+        if (chars[i] !== chars[i + 1]) {
+            s += chars[i]
+            if (numberOfChars > 1) {
+                s += numberOfChars
+            }
+            numberOfChars = 0
+        }
+    }
+
+    // Rewrites chars array; cannot use chars = s.split('')
+    for (let i = 0; i < s.length; i++) {
+        chars[i] = s[i];
+    }
+
+    return s.length
+};
+
+console.log(compress(["a", "a", "b", "b", "c", "c", "c"]));
+console.log(compress(["a"]));
+console.log(compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]));
