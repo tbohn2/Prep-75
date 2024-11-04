4f27fb892e3d4b8c92fbf222545596d6dd69b9ab
tbohn2 <tanner.bohn@yahoo.com>
2024-11-04 12:16:30 -0700

    Improved

--- JavaScript/Array_String/Easy/GreatestCommonDivisorOfStrings.js
+++ JavaScript/Array_String/Easy/GreatestCommonDivisorOfStrings.js
@@ -1,18 +1,29 @@
 // For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
 // Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
 
+// Calculate the GCD of lengths, Get the possible substring from str1, check if the candidate can divide both strings
+// O(n) time (repeating possible divisor and comparing), O(n) space for repeat
 var gcdOfStrings = function (str1, str2) {
-    if (str1.includes(str2)) {
-        const result = str1.replace(str2, '')
-        return result;
+    function gcd(a, b) {
+        while (b !== 0) {
+            let temp = b;
+            b = a % b; // if a is less than b, remainder is a
+            a = temp;
+        }
+        return a;
     }
-    if (str2.includes(str1)) {
-        const result = str2.replace(str1, '')
-        return result;
+
+    const greatestPossLength = gcd(str1.length, str2.length);
+    const possibleDivisor = str1.slice(0, greatestPossLength);
+
+    const numOfRepeats1 = str1.length / greatestPossLength
+    const numOfRepeats2 = str2.length / greatestPossLength
+
+    if (possibleDivisor.repeat(numOfRepeats1) === str1 && possibleDivisor.repeat(numOfRepeats2) === str2) {
+        return possibleDivisor;
     }
-    else {
-        return ''
-    }
+
+    return '';
 };
 
 console.log(gcdOfStrings('abcabc', 'abc'));
