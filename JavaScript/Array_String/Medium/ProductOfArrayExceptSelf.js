ce636342524d8ecdd5266db3f945719fcca517a2
tbohn2 <tanner.bohn@yahoo.com>
2024-11-05 11:38:43 -0700

    Added notes

--- JavaScript/Array_String/Medium/ProductOfArrayExceptSelf.js
+++ JavaScript/Array_String/Medium/ProductOfArrayExceptSelf.js
@@ -3,38 +3,35 @@
 // You must write an algorithm that runs in O(n) time and without using the division operation.
 // O(n) time means time increases linearly with larger array; 
 
-// O(n*n) answer (because loop inside loop for each n):
-// var productExceptSelf = function (nums) {
-//     const result = []
-//     nums.forEach((num, index) => {
-//         let product = 1
-//         nums.forEach((otherNum, i) => {
-//             if (index !== i) {
-//                 product *= otherNum
-//             }
-//         })
-//         result.push(product)
-//     })
-//     return result;
-// };
-
 // O(1) extra space complexity because only the result grows in size with larger nums array and O(n) time because single loops
+// Start at end of array, multiply it by 1 because it is the end, then set that new value equal to the suffixProduct times the nums[i+1] and move one towards start of array
+
+// Initialize answer array with 1s
+// Step 1: Calculate left products
+// answer[i] will hold the product of all elements to the left
+// Update left_product for the next index
+
+// Step 2: Calculate right products
+// Multiply by the product of all elements to the right
+// Update right_product for the next index
+
 var productExceptSelf = function (nums) {
-    const result = new Array(nums.length).fill(1);
+    const n = nums.length;
+    const answer = new Array(n).fill(1);
 
-    // Store each previous product in result array
-    for (i = 1; i < nums.length; i++) {
-        result[i] = result[i - 1] * nums[i - 1];
+    let left_product = 1;
+    for (let i = 0; i < nums.length; i++) {
+        answer[i] = left_product
+        left_product *= nums[i];
     }
 
-    // Start at end of array, multiply it by 1 because it is the end, then set that new value equal to the suffixProduct times the nums[i+1] and move one towards start of array
-    let suffixProduct = 1
-    for (let I = nums.length - 1; I >= 0; I--) {
-        result[I] = result[I] * suffixProduct
-        suffixProduct *= nums[I];
+    let right_product = 1;
+    for (let j = nums.length - 1; j > -1; j--) {
+        answer[j] *= right_product;
+        right_product *= nums[j];
     }
 
-    return result;
+    return answer; // Return the final answer array
 };
 
 
