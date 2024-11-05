1b21d0ad7ea9782754868a412ecb6992ff4190ca
tbohn2 <tanner.bohn@yahoo.com>
2024-11-05 16:36:15 -0700

    Improved efficiency; added comments

--- JavaScript/Stack/Medium/AsteroidCollision.js
+++ JavaScript/Stack/Medium/AsteroidCollision.js
@@ -9,57 +9,31 @@
 // A stack is a data structure that follows the Last In, First Out (LIFO) principle. 
 // This means that the last element added (or "pushed") to the stack will be the first one removed (or "popped") from the stack. 
 
-// Less efficient because splice method is used multiple times; pop is better
-var asteroidCollision = function (asteroids) {
-    let index = 1;
-    while (index < asteroids.length) {
-        if (asteroids[index] < 0 && asteroids[index - 1] > 0) {
-            console.log(asteroids, index);
-            if (Math.abs(asteroids[index]) > Math.abs(asteroids[index - 1])) {
-                asteroids.splice(index - 1, 1)
-            }
-            else if (Math.abs(asteroids[index]) === Math.abs(asteroids[index - 1])) {
-                asteroids.splice(index - 1, 2);
-            } else {
-                asteroids.splice(index, 1);
-            }
-            index--;
-        }
-        else {
-            index++
-        }
-    }
-    return asteroids;
-};
-
 // Using stack
+// If the asteroid is positive or the stack is empty, push it; if there is a stack and asteroid is moving left, compare and push or pop accordingly
 var asteroidCollision = function (asteroids) {
     const stack = [];
 
-    asteroids.forEach((asteroid, index) => {
-        // The top needs to be positive because it will not collide to left whether left is positive or negative; if top is negative, add asteroid to stack no matter what
-        if (stack.length === 0 || asteroid > 0 || stack[stack.length - 1] < 0) {
+    asteroids.forEach(asteroid => {
+        if (asteroid > 0) {
             stack.push(asteroid);
+        } else {
+            while (stack.length > 0 && stack[stack.length - 1] > 0) {
+                const top = stack[stack.length - 1];
+                if (Math.abs(asteroid) > top) {
+                    stack.pop();
+                } else if (Math.abs(asteroid) === top) {
+                    stack.pop();
+                    asteroid = 0;
+                    break;
+                } else {
+                    asteroid = 0;
+                    break;
+                }
+            }
+            if (asteroid !== 0) stack.push(asteroid); // Pushes asteroid that is bigger than top or if stack is empty
         }
-        // The top will be positive, asteroid will be negative
-        while (stack.length !== 0 && asteroid < 0) {
-            const top = stack[stack.length - 1]
-            if (Math.abs(asteroid) < top) {
-                break; // Move on to the next asteroid
-            }
-            else if (Math.abs(asteroid) === top) {
-                stack.pop();
-                break; // Get rid of both
-            }
-            else if (Math.abs(asteroid) > top) {
-                stack.pop();
-            }
-            else if (top < 0) {
-                stack.push(asteroid);
-                break;
-            }
-        }
-    })
+    });
 
     return stack;
 };
--- JavaScript/Two_Pointers/Easy/MoveZeroes.js
+++ JavaScript/Two_Pointers/Easy/MoveZeroes.js
@@ -2,14 +2,22 @@
 
 // Note that you must do this in-place without making a copy of the array.
 
+// Pointer for non-zero int, loop through nums, if number isn't zero, replace at non-zero int pos and move forward in loop, add remaining zeroes to end of array
 var moveZeroes = function (nums) {
-    nums.forEach((num, index) => {
-        if (num === 0) {
-            nums.splice(index, 1)
-            nums.push(num)
+    let nonZeroPos = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[nonZeroPos] = nums[i]
+            nonZeroPos++;
         }
-    });
-    return nums
+    }
+
+    for (let j = nonZeroPos; j < nums.length; j++) {
+        nums[j] = 0;
+    }
+
+    return nums;
 };
 
 console.log(moveZeroes([0, 1, 0, 3, 12]));
