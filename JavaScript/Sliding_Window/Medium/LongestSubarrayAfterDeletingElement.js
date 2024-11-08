56bff20b5131a9e7a7abb541bb62793401863a71
tbohn2 <tanner.bohn@yahoo.com>
2024-11-08 15:17:23 -0700

    Improved clarity; added comment

--- JavaScript/Sliding_Window/Medium/LongestSubarrayAfterDeletingElement.js
+++ JavaScript/Sliding_Window/Medium/LongestSubarrayAfterDeletingElement.js
@@ -2,26 +2,26 @@
 
 // Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
 
+// Count zeroes along the way; move end to nums.length, stopping and moving start forward when 2 zeroes counted
 var longestSubarray = function (nums) {
-    let longestLengthOfSubA = 0;
     let start = 0;
-    let end = 0;
-    let deleted = false;
-    while (end < nums.length) {
-        // deleted ends up being true anyway, so there is more efficient way to do it
+    let maxLength = 0;
+    let zeroes = nums[start] === 0 ? 1 : 0;
+
+    for (let end = 1; end < nums.length; end++) {
         if (nums[end] === 0) {
-            while (deleted) {
-                if (nums[start] === 0) { deleted = false; }
+            zeroes++;
+            while (zeroes > 1) {
+                if (nums[start] === 0) {
+                    zeroes--;
+                }
                 start++;
             }
-            deleted = true
         }
-        end++;
-
-        longestLengthOfSubA = Math.max(longestLengthOfSubA, end - start - 1)
-
+        maxLength = Math.max(end - start, maxLength);
     }
-    return longestLengthOfSubA;
+
+    return maxLength;
 };
 
 console.log(longestSubarray([1, 1, 0, 1]));
