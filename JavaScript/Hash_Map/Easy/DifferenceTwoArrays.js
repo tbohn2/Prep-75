8913e8e916d08ebcbd65d8d0959bd0eb5391a615
tbohn2 <tanner.bohn@yahoo.com>
2024-11-11 14:05:12 -0700

    Added filter method as another solution

--- JavaScript/Hash_Map/Easy/DifferenceTwoArrays.js
+++ JavaScript/Hash_Map/Easy/DifferenceTwoArrays.js
@@ -4,25 +4,29 @@
 // answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
 // Note that the integers in the lists may be returned in any order.
 
-// Sets only contain unique values of any type
+// Sets only contain unique values of any type, thus is set1 and set2, there are no repeated values
 
 var findDifference = function (nums1, nums2) {
     let set1 = new Set(nums1);
     let set2 = new Set(nums2);
 
-    let notInNums2 = new Set
-    let notInNums1 = new Set
+    let uniqueIn1 = [];
+    let uniqueIn2 = [];
 
-    // No need to check if set has num before adding because only unique values are allowed
-    nums1.forEach(num => {
-        if (!set2.has(num)) { notInNums2.add(num) }
+    // const uniqueIn1 = [...set1].filter(num => !set2.has(num)); // Filter method works the same
+    set1.forEach(num => {
+        if (!set2.has(num)) {
+            uniqueIn1.push(num);
+        }
     })
 
-    nums2.forEach(num => {
-        if (!set1.has(num)) { notInNums1.add(num) }
+    set2.forEach(num => {
+        if (!set1.has(num)) {
+            uniqueIn2.push(num);
+        }
     })
 
-    return [Array.from(notInNums2), Array.from(notInNums1)];
+    return [uniqueIn1, uniqueIn2];
 };
 
 console.log(findDifference([1, 2, 3], [2, 4, 6]));
