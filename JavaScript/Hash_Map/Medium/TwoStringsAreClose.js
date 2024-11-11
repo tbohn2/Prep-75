0f308d9e27358bd0a6bb37076a3e741eaee7d174
tbohn2 <tanner.bohn@yahoo.com>
2024-11-11 15:45:03 -0700

    Only used Map solution

--- JavaScript/Hash_Map/Medium/TwoStringsAreClose.js
+++ JavaScript/Hash_Map/Medium/TwoStringsAreClose.js
@@ -7,72 +7,29 @@
 // You can use the operations on either string as many times as necessary.
 
 // Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
-
-// Determine if the unique letters in each word have matching number of occurrences. 
-// If the lengths, letters, and occurrences of unique letters are the same, it is true
+// If the lengths, occurrences, and letters are the same, it is true
 var closeStrings = function (word1, word2) {
-    // Checks length
-    if (word1.length !== word2.length) { return false; }
-
-    let occurrences1 = {};
-    let occurrences2 = {};
-
-    for (const letter of word1) {
-        occurrences1[letter] = (occurrences1[letter] || 0) + 1;
+    if (word1.length !== word2.length) {
+        return false;
     }
 
-    for (const letter of word2) {
-        occurrences2[letter] = (occurrences2[letter] || 0) + 1;
+    let map1 = new Map;
+    let map2 = new Map;
+
+    for (let i = 0; i < word1.length; i++) {
+        map1.set(word1[i], (map1.get(word1[i]) || 0) + 1);
+        map2.set(word2[i], (map2.get(word2[i]) || 0) + 1);
     }
 
-    let numOfOcc1 = new Set;
-    let numOfOcc2 = new Set;
+    if (!Array.from(map1.keys()).every(key => map2.has(key))) {
+        return false
+    };
 
-    for (const letter in occurrences2) {
-        if (occurrences1[letter] && occurrences2[letter]) {      // Checks presence of matching letters
-            numOfOcc1.add(occurrences1[letter])
-            numOfOcc2.add(occurrences2[letter])
-        }
-        else {
-            return false;
-        }
-    }
+    let values1 = Array.from(map1.values()).sort();
+    let values2 = Array.from(map2.values()).sort();
 
-    // Checks occurrences
-    for (const num of numOfOcc2) {
-        if (!numOfOcc1.has(num)) { return false; }
-    }
-
-    return true;
+    return values1.toString() === values2.toString();
 };
-
-// Very similar solution using Map
-// var closeStrings = function (word1, word2) {
-//     if (word1.length !== word2.length) {
-//         return false;
-//     }
-
-//     const countFrequencies = (word) => {
-//         const freqMap = new Map();
-//         for (let char of word) {
-//             freqMap.set(char, (freqMap.get(char) || 0) + 1);
-//         }
-//         return freqMap;
-//     };
-
-//     const freq1 = countFrequencies(word1);
-//     const freq2 = countFrequencies(word2);
-
-//     if (new Set(freq1.keys()).size !== new Set(freq2.keys()).size ||
-//         [...freq1.keys()].some(char => !freq2.has(char))) {
-//         return false;
-//     }
-
-//     const sortedFreq1 = Array.from(freq1.values()).sort((a, b) => a - b);
-//     const sortedFreq2 = Array.from(freq2.values()).sort((a, b) => a - b);
-
-//     return sortedFreq1.join('') === sortedFreq2.join('');
-// };
 
 console.log(closeStrings("abc", "bca"));
 console.log(closeStrings("a", "aa"));
