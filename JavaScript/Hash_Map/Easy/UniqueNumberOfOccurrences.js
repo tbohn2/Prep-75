befe1ded8cc441023bc7d883771fe40656b27872
tbohn2 <tanner.bohn@yahoo.com>
2024-11-11 14:24:36 -0700

    Added more concise solution and comments

--- JavaScript/Hash_Map/Easy/UniqueNumberOfOccurrences.js
+++ JavaScript/Hash_Map/Easy/UniqueNumberOfOccurrences.js
@@ -1,23 +1,32 @@
 // Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
 
+// map object with number and count of repeats in array, new Set of occurences, checking set if occurrences exist and return false if so
 var uniqueOccurrences = function (arr) {
-    let mappedNumOfOccurences = {};
+    let map = {};
 
     arr.forEach(num => {
-        mappedNumOfOccurences[num] = (mappedNumOfOccurences[num] || 0) + 1;
-    });
+        map[num] = (map[num] || 0) + 1;
+    })
 
-    let occurrenceSet = new Set;
-    // For in to iterate over enumerable properties of objects
-    for (let count in mappedNumOfOccurences) {
-        if (occurrenceSet.has(mappedNumOfOccurences[count])) {
+    let occurrences = new Set;
+
+    for (const num in map) {
+        if (occurrences.has(map[num])) {
             return false;
         }
-        occurrenceSet.add(mappedNumOfOccurences[count]);
+        occurrences.add(map[num])
     }
 
     return true;
 };
+
+// Same logic, more concise
+var uniqueOccurrences = function (arr) {
+    const map = arr.reduce((acc, num) => acc.set(num, (acc.get(num) || 0) + 1), new Map());
+    const occurrences = new Set(map.values());
+    return map.size === occurrences.size; // Because sets only contain unique values, this works
+};
+
 
 console.log(uniqueOccurrences([1, 2, 2, 1, 1, 3]));
 console.log(uniqueOccurrences([1, 2]));
