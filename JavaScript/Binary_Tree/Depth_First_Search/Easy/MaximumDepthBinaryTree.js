e410433c54cd7e3496c6baa5c3fdda55d3435aeb
tbohn2 <tanner.bohn@yahoo.com>
2024-11-12 11:57:33 -0700

    Removed null node check

--- JavaScript/Binary_Tree/Depth_First_Search/Easy/MaximumDepthBinaryTree.js
+++ JavaScript/Binary_Tree/Depth_First_Search/Easy/MaximumDepthBinaryTree.js
@@ -9,28 +9,26 @@ function TreeNode(val, left, right) {
     this.right = (right === undefined ? null : right)
 }
 
-let root = new TreeNode(3);
-root.left = new TreeNode(9)
-root.right = new TreeNode(20, 15, 7)
+let root = new TreeNode(3, 9, new TreeNode(20, 15, 7))
 
 // Use stack to store the nodes that need to be tested still, LIFO
 // Preorder transversal; NLR
 var maxDepth = function (root) {
     let max = 0;
-    let stack = [[root, 1]];
+    const stack = [[root, 1]];
 
     while (stack.length > 0) {
-        let [node, depth] = stack.pop(); // We get the left and right of the node, so there is no more info we can get from the node, thus there is no need to push it back onto stack
-        if (node !== null) {
-            max = Math.max(max, depth)
-            if (node.right) {
-                stack.push([node.right, depth + 1])
-            }
-            if (node.left) {
-                stack.push([node.left, depth + 1])
-            }
+        const [node, depth] = stack.pop();
+        max = Math.max(max, depth);
+
+        if (node.right) {
+            stack.push([node.right, depth + 1]);
+        }
+        if (node.left) {
+            stack.push([node.left, depth + 1]);
         }
     }
+
     return max;
 };
 
@@ -52,6 +50,5 @@ var maxDepth = function (root) {
 
 //     return Math.max(leftDepth, rightDepth) + 1;
 // };
-
 
 console.log(maxDepth(root));
